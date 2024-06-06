b2dcd7f4794c845114ad14bf234883a20547924e
CALEB KOMEN <komencaleb502@gmail.com>
2024-06-06 09:42:16 +0300

    code along

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,19 @@
 class MySet:
+    def __init__(self,enumarable = []):
+        self.dictionary = {}
+        for value in enumarable:
+            self.dictionary[value] = True
+    def has(self, value):
+        return value in self.dictionary
+    
+    def add(self, value):
+        self.dictionary[value] = True # Add a value as a key on the Dictionary
+        return self 
 
-    pass
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self  
+
+
+  
+  
